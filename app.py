8abd223a3c473046289851dc057d0e1e08b65dd0
John Allison <jrallison@gmail.com>
2019-07-12 20:05:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+import time
+import automationhat
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'Hello world <a href="/water">Water plants</a>'
+
+@app.route('/water')
+def water():
+    automationhat.relay.one.on()
+    time.sleep(5)
+    automationhat.relay.one.off()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
